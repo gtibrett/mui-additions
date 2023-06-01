a6157b775accd1a0d4a00e0acf5ec7dc59693068
Brett Harris <gtibrett@gmail.com>
2023-06-01 12:08:47 -0400

    Updated README.md and dependencies for new components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
-export {default as Breakpoints } from './Breakpoints';
+export {default as Breakpoints} from './Breakpoints';
+export {default as Link} from './Link';
+export {default as SkipNav} from './SkipNav';
+export {default as UkraineButton} from './UkraineButton';
 export {default as useComponentDimensionsWithRef} from './useComponentDimensionsWithRef';
+export {default as usePageTitle} from './usePageTitle';
 export * from './jest';
