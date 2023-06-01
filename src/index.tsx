7a8570b6bc1d69a44e6aaf4d3424126262a0393c
Brett Harris <gtibrett@gmail.com>
2023-06-01 13:00:17 -0400

    add missing components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 export {default as Breakpoints} from './Breakpoints';
+export {default as Dialog} from './Dialog';
 export {default as Link} from './Link';
 export {default as SkipNav} from './SkipNav';
+export {default as Tabs, TabContent} from './Tabs';
 export {default as UkraineButton} from './UkraineButton';
 export {default as useComponentDimensionsWithRef} from './useComponentDimensionsWithRef';
 export {default as usePageTitle} from './usePageTitle';
