6130360cf250a4a0763210e7cff300a0df5808eb
Brett Harris <gtibrett@gmail.com>
2024-03-30 23:28:52 -0400

    update tests

--- src/UkraineButton.test.tsx
+++ src/UkraineButton.test.tsx
@@ -3,8 +3,14 @@ import {testForAccessibility} from './jest';
 import UkraineButton from './UkraineButton';
 
 describe('UkraineButton.tsx', () => {
-	test('Render', async () => {
+	test('Render: Portal', async () => {
 		render(<UkraineButton/>);
+		
+		expect(screen.getByLabelText(/Stand with Ukraine/i)).toBeInTheDocument();
+	});
+	
+	test('Render: No Portal', async () => {
+		render(<UkraineButton noPortal/>);
 		
 		expect(screen.getByLabelText(/Stand with Ukraine/i)).toBeInTheDocument();
 	});
